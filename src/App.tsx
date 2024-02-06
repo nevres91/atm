ba106c56f3816d1c221924f27c137d0e9b60077f
Nevres <nevres_muratovic@hotmail.com>
2024-02-06 18:20:30 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import bg from "./img/bg.jpeg";
+import enter from "./img/enter.jpeg";
+import receipt from "./img/receipt2.png";
+import card from "./img/card2.png";
+import envelope from "./img/envelope2.png";
+import money from "./img/money2.png";
+import Keypad from "./components/Keypad";
+
+const alignItems = {
+  justifyContent: " center",
+  alignItems: "center",
+  textAlign: "center",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      sx={{
+        display: "flex",
+        background: "rgb(145,146,140)",
+        height: "100vh",
+        maxHeight: "100vh",
+        justifyContent: "center",
+        // overflow: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          background: "rgb(145,146,140)",
+          width: "100%",
+          ...alignItems,
+        }}
+      >
+        <Card
+          sx={{ height: "350px", width: "200px", border: "3px solid black" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CardMedia sx={{ height: "300px", width: "200px" }} image={enter} />
+          <CardActions
+            sx={{
+              ...alignItems,
+            }}
+          >
+            <Typography sx={{ fontWeight: "900" }}>ENTER THE BANK</Typography>
+          </CardActions>
+        </Card>
+
+        <Keypad />
+      </Box>
+      <Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            height: "85vh",
+            width: "85vh",
+            minWidth: "90vh",
+            backgroundColor: "red",
+            margin: "auto",
+            marginTop: "0",
+            background: `url(${bg}) no-repeat center center/cover`,
+            textAlign: "center",
+          }}
+        >
+          <Typography color={"#d3d3d3"} my={5} mx={4} variant="h3">
+            INTERNATIONAL BANK
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            height: "15vh",
+            ...alignItems,
+          }}
+        >
+          <Button
+            sx={{
+              height: "95%",
+              width: "83%",
+              background: `url(${money}) no-repeat center center/cover`,
+            }}
+          />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+          background: "rgb(145,146,140)",
+          width: "100%",
+          ...alignItems,
+        }}
+      >
+        <Button
+          sx={{
+            borderRadius: "10px",
+            // boxShadow: "0px 0px 17px 6px rgba(21,255,0,0.75)",
+            marginTop: "3%",
+            height: "24vh",
+            width: "30vh",
+            background: `url(${receipt}) no-repeat center center/cover`,
+          }}
+        />
+        <Button
+          sx={{
+            borderRadius: "10px",
+            // boxShadow: "0px 0px 17px 6px rgba(246,72,101,0.75)",
+            marginTop: "12%",
+            height: "38vh",
+            width: "30vh",
+            background: `url(${card}) no-repeat center center/cover`,
+          }}
+        />
+        <Button
+          sx={{
+            borderRadius: "10px",
+            // boxShadow: "0px 0px 17px 6px rgba(21,255,0,0.75)",
+            marginTop: "10%",
+            height: "20vh",
+            width: "30vh",
+            background: `url(${envelope}) no-repeat center center/cover`,
+          }}
+        />
+      </Box>
+    </Box>
   );
 }
 
---
+++ src/components/Keypad.tsx
@@ -0,0 +1,79 @@
+import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import React from "react";
+
+const buttonStyle = (fSize = "40px", bgColor = "#c8c8c8") => ({
+  display: "flex",
+  background: bgColor,
+  width: "100%",
+  height: "100%",
+  boxShadow: "inset -5px -5px 3px #3f3f3f, inset 5px 5px 3px #fdfdfd",
+  borderRadius: "5px",
+  justifyContent: "center",
+  alignItems: "center",
+  textAlign: "center",
+  fontSize: fSize,
+  fontWeight: "900",
+  color: "black",
+  padding: "0",
+});
+
+const Keypad = () => {
+  return (
+    <Grid
+      sx={{ width: "90%", height: "40%", marginTop: "15%" }}
+      container
+      spacing={2}
+    >
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>1</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>2</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>3</Button>
+      </Grid>
+      <Grid item xs={3.9}>
+        <Button sx={buttonStyle("20px", "#f54b67")}>CANCEL</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>4</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>5</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>6</Button>
+      </Grid>
+      <Grid item xs={3.9}>
+        <Button sx={buttonStyle("20px", "#f2ff60")}>CLEAR</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>7</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>8</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>9</Button>
+      </Grid>
+      <Grid item xs={3.9}>
+        <Button sx={buttonStyle("20px", "#4db052")}>ENTER</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}></Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}>0</Button>
+      </Grid>
+      <Grid item xs={2.7}>
+        <Button sx={buttonStyle()}></Button>
+      </Grid>
+      <Grid item xs={3.9}>
+        <Button sx={buttonStyle()}></Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Keypad;
